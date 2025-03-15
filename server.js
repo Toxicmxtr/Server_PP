e429ad336ecfb121957bec57e2f055e366263fae
Toxicmxtr <danixnavi06@gmail.com>
2025-03-15 21:02:55 +0300

    дурак

--- server.js
+++ server.js
@@ -499,7 +499,7 @@ app.get('/posts', async (req, res) => {
         post_date: post.post_date,
         post_time: post.post_time,
         post_views: post.post_views,
-        post_picture: post.post_picture ? `http://localhost:3000/posts/${post.post_picture}` : null,
+        post_picture: post.post_picture ? `http://89.104.66.135:3000/posts/${post.post_picture}` : null,
         user_name: post.user_name || 'Неизвестный пользователь',
         user_acctag: post.user_acctag || '@Неизвестный',
         avatar_url: post.avatar_url || null,
@@ -893,5 +893,5 @@ app.post('/upload-avatar/:id', upload.single('avatar'), async (req, res) => {
 
 // Запуск сервера
 app.listen(port, () => {
-  console.log(`Сервер работает на http://localhost:${port}`);
+  console.log(`Сервер работает на http://89.104.66.135:${port}`);
 });
