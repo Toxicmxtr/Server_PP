e749dfa88fba58c0b1c3cf04976713218941876f
Toxicmxtr <danixnavi06@gmail.com>
2025-03-17 12:00:19 +0300

    добавлена проверка

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
+
 // Раздача статических файлов
 app.use('/.well-known', express.static(path.join(__dirname, '.well-known')));
 
